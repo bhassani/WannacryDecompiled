ad4408fc91f0ca1a7ca0a12bea08b8498e220670
bhassani <bhassani@users.noreply.github.com>
2020-07-17 15:09:21 -0500

    Update WinMain.cpp

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,3 +1,6 @@
+//https://medium.com/@yogeshojha/reverse-engineering-wannacry-ransomware-using-ghidra-finding-the-killswitch-a212807e9354
+//https://blog.malwarebytes.com/threat-analysis/2017/05/the-worm-that-spreads-wanacrypt0r/
+
 #include <Windows.h>
 #include <wininet.h>
 #pragma comment(lib, "wininet.lib")
@@ -7,24 +10,21 @@ int RealMain()
   //check Args
 }
 
-int checkUrl()
-{
-  HANDLE hInt;
-  HANDLE hUrl;
-  hInt = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
-  hUrl = InternetOpenUrl(hInt, L"http://www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com", NULL, 0, INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE, NULL);
-  if (hUrl)
-  {
-    InternetCloseHandle(hUrl);
-    ExitProcess(0);
-  }
-  else {
-    RealMain();
-  }
-  InternetCloseHandle(hInt);
-}
-
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
+    char szUrl[] = "http://www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com";
+    HINTERNET hInternet;
+    HINTERNET hUrl;
+    hInternet = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    hUrl = InternetOpenUrl(hInternet, szUrl, NULL, 0, INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE, NULL);
+    if (hUrl)
+    {
+      InternetCloseHandle(hUrl);
+      ExitProcess(0);
+    }
+    else {
+      RealMain();
+    }
+    InternetCloseHandle(hInternet);
     return 0;
 }
