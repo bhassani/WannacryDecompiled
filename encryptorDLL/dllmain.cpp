3d9930e9f2684cba9b1b43f821f03ed0b40998af
bhassani <bhassani@users.noreply.github.com>
2026-09-11 12:44:43 +0200

    Update dllmain.cpp
    fix comments

--- encryptorDLL/dllmain.cpp
+++ encryptorDLL/dllmain.cpp
@@ -1,10 +1,6 @@
 #include <Windows.h>
 
-//help from:
-//https://www.youtube.com/watch?v=ru5VzUigKqw
-
-//compile and embed in main project
-//this file is loaded by Wannacry for the encryption portion
+//help from: https://www.youtube.com/watch?v=ru5VzUigKqw
 
 BOOL WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID)
 {
@@ -12,16 +8,13 @@ BOOL WINAPI DllMain(HINSTANCE hModule, DWORD dwReason, LPVOID)
     switch (dwReason)
     {
     case DLL_PROCESS_ATTACH:
-    {
         MessageBoxA(NULL, "DLL_PROCESS_ATTACH", "DLL_PROCESS_ATTACH", MB_OK);
-    }
-    break;
+        break;
     case DLL_PROCESS_DETACH:
-    {
-        //detach
-    }
-    break;
+        MessageBoxA(NULL, "DLL_PROCESS_DETACH", "DLL_PROCESS_DETACH", MB_OK);
+        break;
     case DLL_THREAD_ATTACH:
+        break;
     case DLL_THREAD_DETACH:
         break;
     }
